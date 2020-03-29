b15c28b79058433935e251c3be54c5a083e26770
13020180073 <61999365+13020180073@users.noreply.github.com>
2020-03-29 18:25:58 +0700

    Add files via upload

---
+++ PBO TGS2/bio.java
@@ -0,0 +1,30 @@
+// mengimpor Scanner ke program
+import java.util.Scanner;
+
+public class bio {
+
+    public static void main(String[] args) {
+        // deklarasi variabel
+        String nama, jurusan,fakultas, nim;
+        
+
+        // membuat scanner baru
+        Scanner keyboard = new Scanner(System.in);
+
+        // Tampilkan output ke user
+     
+        System.out.print("Nama: ");
+        nama = keyboard.nextLine();
+        System.out.print("Jurusan: ");
+        jurusan = keyboard.nextLine();
+        System.out.print("Fakultas: ");
+        fakultas = keyboard.nextLine();
+        System.out.print("Nim: ");
+        nim = keyboard.nextLine();
+       
+
+
+       
+    }
+
+}
